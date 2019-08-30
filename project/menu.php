6656a6ea9934b401ad316d029c9eb5292d7107b5
rootadmin Macbook <palm2530@gmail.com>
2019-08-30 11:01:06 +0700

    test

--- project/menu.php
+++ project/menu.php
@@ -4,7 +4,7 @@
             <li class="nav-item">
                 <a class="nav-link active" href="#">
                     <span data-feather="home"></span>
-                    Dashboard <span class="sr-only">(current)</span>
+                    test <span class="sr-only">(current)</span>
                 </a>
             </li>
             <li class="nav-item">
